b0510a4553a5a30d4f1bc983555b5b81b55dbe3e
Robert Wiesner <robert.wiesner@reev.com>
2022-11-24 11:57:13 +0100

    docs: add --debug and hint for fix

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Reproducer https://github.com/detekt/detekt/issues/5247
 # HowTo
 - Install https://direnv.net/
 - Run `direnv allow`
-- Run `./scripts/runDetekt.sh`
+- Run `./scripts/runDetekt.sh` -> it should fail
+- Removing `--build-upon-default-config` in  `./scripts/runDetekt.sh` should fix the issue
--- scripts/runDetekt.sh
+++ scripts/runDetekt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DETEKT_VERSION=$(detekt --version)
-detekt --auto-correct --max-issues 0 --input src --build-upon-default-config --config detekt/detekt.yaml --plugins detekt/plugins/detekt-formatting-$DETEKT_VERSION.jar
+detekt --debug --auto-correct --max-issues 0 --input src --build-upon-default-config --config detekt/detekt.yaml --plugins detekt/plugins/detekt-formatting-$DETEKT_VERSION.jar
